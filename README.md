22c53732001c97c79e9c08e14a070ce8bbb528e2
Themwhiskers <zvika.sage@gmail.com>
2023-10-09 21:53:56 +0300

    leason 8 - react auth start

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 follow along project - resources & guidness by Net Ninja on youtube:
 
-**(https://youtu.be/98BzS5Oz5E4?si=5ldSFOZkRgKLPhQs)**
+**Part 1: (https://youtu.be/98BzS5Oz5E4?si=5ldSFOZkRgKLPhQs)**
 
-last time stemp:  video no. 12 : 00:25
+**Part 2: (https://www.youtube.com/watch?v=WsRBmwNkv3Q&list=PL4cUxeGkcC9g8OhpOZxNdhXggFz2lOuCT&ab_channel=NetNinja)**
+
+part 1 - done
+
+part 2 - leason 8 - 00:00
--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const workoutsRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 const mongoose = require('mongoose');
 require('dotenv').config();
 // middleware:
@@ -11,6 +12,7 @@ app.use((req,res,next) => {
     next();})
 // routes:
 app.use('/api/workouts',workoutsRoutes);
+app.use('/api/user/', userRoutes);
 //connect to DB:
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
